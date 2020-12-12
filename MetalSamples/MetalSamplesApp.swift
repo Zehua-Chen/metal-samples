84f15181aecbf164dc01a74ccad309366610f590
Zehua-Chen <peterchen06242000@outlook.com>
2020-12-12 14:50:19 -0600

    [feat] Setup Metal Samples App

---
+++ MetalSamples/MetalSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalSamplesApp.swift
+//  MetalSamples
+//
+//  Created by Zehua Chen on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MetalSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
