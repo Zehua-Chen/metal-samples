0fe8aa5602fd325d540ad7787c55056b72ba2bbc
Zehua-Chen <peterchen06242000@outlook.com>
2020-12-20 18:11:08 -0600

    [fix] Fix Signing

--- SampleSkybox/SampleSkybox.swift
+++ SampleSkybox/SampleSkybox.swift
@@ -21,6 +21,8 @@ public class SampleSkybox: Sample {
   }
 
   public override var tag: SampleTag {
+//    let bundle = SampleSkybox._bundle
+//    print(SampleSkybox._bundle.url(forResource: nil, withExtension: "obj"))
     return .textures
   }
 
